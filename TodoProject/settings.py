0366529b7e71aaa456c47678dc63f6a83b50a0e6
Zahid Hasan Milu <minions.milu889@gmail.com>
2024-09-03 12:22:18 +0600

    requirements.txt

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles/'
-STATICFILES_DIRS =[
-    BASE_DIR, 'static',
-]
+# STATICFILES_DIRS =[
+#     BASE_DIR, 'static',
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
